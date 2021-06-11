0a4ed62737437fc06925ba093e0103a752ceece9
Leo16012000 <67458956+Leo16012000@users.noreply.github.com>
2021-06-11 22:24:47 +0700

    all okay

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,18 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
 
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["bookmarks"],
+};
+
+rootReducer = combineReducers({
+  booksReducer: persistReducer(persistConfig, booksReducer),
+});
 // Note: this API requires redux@>=3.1.0
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store);
